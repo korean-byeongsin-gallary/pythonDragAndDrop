c496e3982b8cb1213bbc9298679a16809ed33c11
DESKTOP-L18T2U1\dream <dreamworkerw@naver.com>
2020-11-17 09:01:02 +0900

    indentBlock

--- dragAndDrop.py
+++ dragAndDrop.py
@@ -13,6 +13,7 @@ class block(QWidget):
         self.offset = 0
         self.code = code
         self.layout = QHBoxLayout()
+        self.layout.setContentsMargins(10,10,5,5)
         self.title = QLabel(title)
         self.text = QLineEdit()
         self.btn = QPushButton("aaaa")
@@ -25,10 +26,15 @@ class block(QWidget):
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.red)
         self.setPalette(p)
+        self.setMaximumHeight(40)
+        self.setMinimumHeight(40)
+        self.setMaximumWidth(210)
+        self.setMinimumWidth(210)
 
     def mouseMoveEvent(self, e: QMouseEvent):
         mime_data = QMimeData()
         self.setVisible(False)
+        self.setParent(None)
         mime_data.setData("application/hotspot", b"%d %d %d" % (e.x(), e.y(), self.code))
 
         drag = QDrag(self)
@@ -42,6 +48,84 @@ class block(QWidget):
 
         drag.setHotSpot(e.pos() - self.rect().topLeft())
         drag.exec_(Qt.MoveAction)
+
+class indentBlock(QWidget):
+    def __init__(self, title, _parent,code):
+        QWidget.__init__(self, parent= _parent,flags=Qt.Widget)
+        self.offset = 0
+        self.code = code
+        self.container = QVBoxLayout()
+        self.indenting = QHBoxLayout()
+        self.codeSpace = QVBoxLayout()
+        self.layout = QHBoxLayout()
+        self.title = QLabel(title)
+        self.text = QLineEdit()
+        self.topSpace = QSpacerItem(0, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.bottomSpace = QSpacerItem(0, 50, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.indentSpace = QSpacerItem(30, 0, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        #self.btn = QPushButton("aaaa")
+        self.layout.addWidget(self.title)
+        self.layout.addWidget(self.text)
+        #self.layout.addWidget(self.btn)
+        self.indenting.addItem(self.indentSpace)
+        #self.codeSpace.addWidget()
+        self.indenting.addLayout(self.codeSpace)
+        self.container.addLayout(self.layout)
+        #self.container.addItem(self.topSpace)
+        self.container.addLayout(self.indenting)
+        self.container.addItem(self.bottomSpace)
+        self.setLayout(self.container)
+        #배경색 설정
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.blue)
+        self.setPalette(p)
+        self.setMaximumWidth(250)
+        self.setMinimumWidth(250)
+        self.codeSpace.setSpacing(0)
+    def insertBlock(self, block, pos):
+        #self.codeSpace.addWidget(block)
+        self.codeSpace.insertWidget(pos, block)
+
+    def blockCount(self):
+        return self.codeSpace.count()
+
+    def deleteBlock(self,block):
+        pass
+
+
+    def mouseMoveEvent(self, e: QMouseEvent):
+        mime_data = QMimeData()
+        self.setVisible(False)
+        mime_data.setData("application/hotspot", b"%d %d %d" % (e.x(), e.y(), self.code))
+  
+
+        drag = QDrag(self)
+        drag.parent = self
+        # MIME 타입데이터를 Drag에 설정
+        drag.setMimeData(mime_data)
+        # 드래그시 위젯의 모양 유지를 위해 QPixmap에 모양을 렌더링
+        pixmap = QPixmap(self.size())
+        self.render(pixmap)
+        drag.setPixmap(pixmap)
+
+        drag.setHotSpot(e.pos() - self.rect().topLeft())
+        drag.exec_(Qt.MoveAction)
+
+'''
+    def dropEvent(self, e: QDropEvent):
+        source = e.source()
+        print(source)
+        # 보내온 데이터를 받기
+        # 그랩 당시의 마우스 위치값을 함께 계산하여 위젯 위치 보정
+        offset = e.mimeData().data("application/hotspot")
+        x, y, code = offset.data().decode('utf-8').split()
+        #self.blocks[int(code)].setParent(self)
+        self.blocks[int(code)].setVisible(True)
+
+        e.setDropAction(Qt.MoveAction)
+        e.accept()
+'''
 
 
 class blockSpawn(QWidget):
@@ -59,13 +143,14 @@ class blockSpawn(QWidget):
 
     def mouseMoveEvent(self, e: QMouseEvent):
         newBlock = self.window.addBlock(self.title,(self.x(),self.y()))
+        newBlock.setParent(None)
         mime_data = QMimeData()
         mime_data.setData("application/hotspot", b"%d %d %d" % (e.x(), e.y(), newBlock.code))
         drag = QDrag(newBlock)
         drag.parent = newBlock
 
         drag.setMimeData(mime_data)
-        newBlock.setVisible(True)
+        #newBlock.setVisible(True)
         pixmap = QPixmap(newBlock.size())
         newBlock.render(pixmap)
         drag.setPixmap(pixmap)
--- main.py
+++ main.py
@@ -16,15 +16,25 @@ class mainWindow(QMainWindow, form_class) :
         super().__init__()
         self.setupUi(self)
         self.blocks = list()
+        #self.addBlock('asdf', (0, 0))
         self.addBlock('sasdf',(50,500))
-        self.addBlock('sssss',(30,30))
+        self.addBlock('sssss',(500,50))
+        self.addIndentBlock('ddd',(200,200))
+        print(self.blocks)
         dragAndDrop.blockSpawn('asdf',self.centralwidget,self)
+
         self.initWidget()
+
     
     def addBlock(self,name,pos):
         block = dragAndDrop.block(name,self.centralwidget,len(self.blocks))
         self.blocks.append(block)
         block.move(pos[0],pos[1])
+        return block
+    def addIndentBlock(self,name,pos):
+        block = dragAndDrop.indentBlock(name, self.centralwidget, len(self.blocks))
+        self.blocks.append(block)
+        block.move(pos[0], pos[1])
         return block
         
 
@@ -39,9 +49,20 @@ class mainWindow(QMainWindow, form_class) :
         # 보내온 데이터를 받기
         # 그랩 당시의 마우스 위치값을 함께 계산하여 위젯 위치 보정
         offset = e.mimeData().data("application/hotspot")
-        x, y, code = offset.data().decode('utf-8').split() 
+        x, y, code = offset.data().decode('utf-8').split()
+        #indentBlock 안에 drop했다면
+        if self.blocks[2].x() <= e.pos().x() <= self.blocks[2].x() + self.blocks[2].width()  and self.blocks[2].y() <= e.pos().y() <= self.blocks[2].y() + self.blocks[2].height():
+            self.blocks[2].insertBlock(self.blocks[int(code)], (e.pos().y() - self.blocks[2].y()) // 50)
+            self.blocks[2].setMinimumHeight(self.blocks[2].blockCount() * 50 + 50)
+            self.blocks[2].setMaximumHeight(self.blocks[2].blockCount() * 50 + 50)
+        else:
+            self.blocks[int(code)].setParent(self)
+            self.blocks[2].setMinimumHeight(self.blocks[2].blockCount() * 50 + 50)
+            self.blocks[2].setMaximumHeight(self.blocks[2].blockCount() * 50 + 50)
         self.blocks[int(code)].move(position - QPoint(int(x), int(y)))
         self.blocks[int(code)].setVisible(True)
+        print(self.blocks[2].codeSpace.itemAt(0))
+
 
         e.setDropAction(Qt.MoveAction)
         e.accept()
